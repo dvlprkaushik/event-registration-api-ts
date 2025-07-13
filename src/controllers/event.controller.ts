b818770eddeb2b6f0a3115627e54ee765af0c627
dvlpr_kaushik <103560296+dvlprkaushik@users.noreply.github.com>
2025-07-13 19:47:24 +0530

    ✨ Add createEvent controller

---
+++ src/controllers/event.controller.ts
@@ -0,0 +1,35 @@
+import type { Event, EventBody } from "@/types/Event.types.js";
+import { MSG, STCODE } from "@/types/HttpUtils.types.js";
+import type { RequestHandler } from "express";
+
+const EVENTS: Event[] = [];
+export const createEvent: RequestHandler<{}, {}, EventBody> = (req, res) => {
+  try {
+    const { fullName, email, phone, eventName } = req.body;
+    if (EVENTS.some((e) => e.email === email)) {
+      return res.status(STCODE.BAD_REQUEST).json({
+        success: false,
+        message: MSG.BAD_REQUEST,
+      });
+    }
+    const newEvent: Event = {
+      fullName,
+      email,
+      phone,
+      eventName,
+      timestamp: new Date().toISOString(),
+    };
+    EVENTS.push(newEvent);
+    return res.status(STCODE.CREATED).json({
+      success: true,
+      message: "Registration successful",
+      data: newEvent,
+    });
+  } catch (error) {
+    return res.status(STCODE.INTERNAL_SERVER_ERROR).json({
+      success: true,
+      message: MSG.INTERNAL_SERVER_ERROR,
+      error: error,
+    });
+  }
+};
--- src/types/Event.types.ts
+++ src/types/Event.types.ts
@@ -2,7 +2,7 @@ import type { eventSchema } from "@/validators/event.validator.js";
 import type { z } from "zod";
 
 export type EventBody = z.infer<typeof eventSchema>;
-export type Event = EventBody & { timestamp: Date };
+export type Event = EventBody & { timestamp: string };
 
 // User types
 export type UserType = { role: "admin" | "user" };
