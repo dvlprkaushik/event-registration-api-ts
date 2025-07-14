29eab48e48a52816c3f6b0fd817b5461f30b2a1e
dvlpr_kaushik <103560296+dvlprkaushik@users.noreply.github.com>
2025-07-14 12:37:09 +0530

    🔧 Fix: add start script for Render deployment

--- src/middlewares/checkRole.middleware.ts
+++ src/middlewares/checkRole.middleware.ts
@@ -2,15 +2,15 @@ import type { Role } from "@/types/Event.types.js";
 import { MSG, STCODE } from "@/types/HttpUtils.types.js";
 import type { RequestHandler } from "express";
 
-export const checkRole = (...allowedRoles : Role[]): RequestHandler => {
-    return (req, res, next) => {
-        const role = req.user?.role;
-        if (!role || !allowedRoles.includes(role)) {
-            return res.status(STCODE.UNAUTHORIZED).json({
-                success: false,
-                message: `Access denied. ${MSG.UNAUTHORIZED}`
-            });
-        }
-        next();
-    };
+export const checkRole = (...allowedRoles: Role[]): RequestHandler => {
+  return (req, res, next) => {
+    const role = req.user?.role;
+    if (!role || !allowedRoles.includes(role)) {
+      return res.status(STCODE.UNAUTHORIZED).json({
+        success: false,
+        message: `Access denied. ${MSG.UNAUTHORIZED}`,
+      });
+    }
+    next();
+  };
 };
